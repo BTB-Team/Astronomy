7cc0c99ff3223bbe4e01fc64656e4f40c9dcd35c
nazifaRahimi <nazifarahimy6@gmail.com>
2026-05-12 22:19:38 +0430

    chore: fix base path and outDir in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.dev
 export default defineConfig({
   plugins: [react()],
+  base: '/Astronomy/', 
+  build: {
+    outDir: 'dist' 
+  }
 })
