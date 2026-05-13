a992d8db9fc0b5e881606dd88d574eee5044fc4b
nazifaRahimi <nazifarahimy6@gmail.com>
2026-05-13 23:43:48 +0430

    merge hero section

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react' // اصلاح شد
+import react from '@vitejs/plugin-react' 
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
-  resolve: {
-    alias: {
-      // این خط باعث می‌شود تمام ایمپورت‌های react-native به نسخه وب تبدیل شوند
-      'react-native': 'react-native-web',
+    plugins: [
+        tailwindcss(),
+        react(),
+    ],
+    base: '/Astronomy/', 
+    build: {
+        outDir: 'docs'
     },
-  },
+    resolve: {
+        alias: {
+            'react-native': 'react-native-web',
+        },
+    },
 })
