70ac1972d0eda8811a8bbef78c6adecd754b4764
nazifaRahimi <nazifarahimy6@gmail.com>
2026-06-17 16:41:18 +0430

    feat: connect home features to blog posts

--- src/components/home/Features.jsx
+++ src/components/home/Features.jsx
@@ -1,54 +1,39 @@
 import { useTranslation } from "react-i18next";
-import image3 from "../../assets/images/img3.webp";
-import image4 from "../../assets/images/img4.webp";
-import image5 from "../../assets/images/img5.webp";
+import { Link } from "react-router-dom";
+import { productPost } from "../../data/productPost";
 
 export default function Features() {
   const { t } = useTranslation();
-  const items = [
-    {
-      title:t("features.items.0.title"),
-      description: t("features.items.0.description"),
-      img: image3,
-    },
-    {
-      title:t("features.items.1.title"),
-      description: t("features.items.1.description"),
-      img: image4,
-    },
-    {
-      title:t("features.items.2.title"),
-      description: t("features.items.2.description"),
-      img: image5,
-    }
-  ];
+  const latestPosts = [...productPost].slice(-3).reverse();
   return (
     <section className="py-20 mt-20 px-6 md:px-20">
       <h2 className="text-3xl font-bold text-center mb-10">
         {t("features.mainTitle")}
       </h2>
       <div className="grid md:grid-cols-3 gap-6">
-        {items.map((item, i) => (
-          <div
-            key={i}
-            className="bg-white/5 rounded-xl overflow-hidden shadow-lg"
+        {latestPosts.map((post) => (
+          <Link
+             key={post.id}
+            to={`/blog/${post.id}`}
+            className="bg-white/5 rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 transition duration-300 block"
+            
           >
             {/* Image */}
             <img
-              src={item.img}
-              alt={item.title}
+              src={post.image}
+              alt={t(`blog.posts.${post.title}`)}
               className="h-48 w-full object-cover"
             />
             {/* Content */}
             <div className="p-4">
               <h3 className="text-xl font-semibold">
-                {item.title}
+                 {t(`blog.posts.${post.title}`)}
               </h3>
               <p className="text-gray-400 text-sm mt-2">
-                {item.description}
+              {t(`blog.posts.${post.desc}`).slice(0, 120)}...
               </p>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
